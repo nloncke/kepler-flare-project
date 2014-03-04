3bf5f3da1ad4ced21b8e20e59ca6697d2996083d
Lucianne Walkowicz <l.m.walkowicz@gmail.com>
2014-03-04 11:52:00 -0500

    added practice files, made pseudocode less pseudo

--- flare_feature_calc.py
+++ flare_feature_calc.py
@@ -1,35 +1,60 @@
 import numpy as np
 from scipy import stats
+from lightcurves import *
+import itertools
 
-# read in light of flare flag files
+#def shift(l, n):
+#    return l[n:] + l[:n]
 
-# read in list of lightcurves that have flagged flares
+def flareFeatures(files, flarfiles)
+    """ Given a list of lightcurve files and flag files, 
+    compute an array of features for each flare event"""
+    # read in list of flare flag files
+    with open(flarfiles, 'r') as f:
+        myFlags = [line.strip() for line in f]
+    f.close()
 
-for i in range(len(filelist)):
+    # read in list of lightcurve files
+    with open(files, 'r') as f:
+        myFiles = [line.strip() for line in f]
+    f.close()
 
-    # read in lightcurve & normalize to 1
-    lightcurve = np.genfromtext(lightcurvefilename)
-    time = lightcurve[:, 0]
-    flux = lightcurve[:, 1]
-    normflux = flux / np.median(flux)
+    flags = getflags(flarfiles)
+    
+    for i in range(len(files)):
 
-    # read in associated flare flags - use getFlags?
+        # read in lightcurve & normalize to 1
+        lightcurve = np.genfromtext(files[i])
+        time = lightcurve[:, 0]
+        flux = lightcurve[:, 1]
+        normflux = flux / np.median(flux)
+        
+        # grab array of flagged indices
+        lcflags = flags[i]
+        floatflags = [float(j) for j in lcflags]
+
+        # find boundaries bt events
+        bnds = floatflags - np.roll(floatflags, 1)
+        # indices --> having some confusion about tuples vs indices here
+        tst = np.where(bnds != 1.)
+
+        # compute descriptive stats (skew, kurtosis) for each flare 
+        flareSkew = stats.skew(normflux)
+        flareKurt = stats.kurtosis(normflux)
+
+        # compute 2nd derivative of points flagged as flare points
 
 
-    # compute descriptive stats (skew, kurtosis) for each flare 
-    flareSkew = stats.skew
-    flareKurt = stats.kurtosis
 
-# compute 2nd derivative of points flagged as flare points
+        # test whether first and last points are lower than the midpoint
 
-# test whether first and last points are lower than the midpoint
+        # compute the abs value of the slope around the flare
 
-# compute the abs value of the slope around the flare
+        # compute the ratio of the abs value of the slopes on either side of the flare
 
-# compute the ratio of the abs value of the slopes on either side of the flare
+        # compute the amplitude of the full lightcurve (w stellar variabiity)
 
-# compute the amplitude of the full lightcurve (w stellar variabiity)
+        # compute the stddev of the flattened lightcurve 
+        # (w stellar variability subtracted)
 
-# compute the stddev of the flattened lightcurve 
-# (w stellar variability subtracted)
-
+    return flareFeatureArray
